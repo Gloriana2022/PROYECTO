54a98712ccd6e961a82559fe4b8f193c0b8376fb
estebanmata <ematamoros1989@hotmail.com>
2022-04-03 17:10:44 -0600

    se agrega mensaje

--- client/src/app/components/gestionar-vehiculo/gestionar-vehiculo.component.ts
+++ client/src/app/components/gestionar-vehiculo/gestionar-vehiculo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from 'src/app/models/vehiculo.model';
+
 
 @Component({
   selector: 'app-gestionar-vehiculo',
@@ -7,9 +9,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionarVehiculoComponent implements OnInit {
 
-  constructor() { }
+  //Lista de chofer
+  listaEstados : Vehiculo[] = [];
+
+  //Configuración de la tabla
+  displayedColumns: string[] = ['identificador', 'Anno', 'Modelo','Placa','Color', 'Puntuacion', 'Estado','UbicacionActual'];
+  dataSource!:  MatTableDataSource<any>;
+
+
+  //Para la paginación
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor( private estadoService : EstadoService, private _snackbar: MatSnackBar, private router: Router ) { }
 
   ngOnInit(): void {
+    this.consultarEstado();
   }
+
+  ngAfterViewInit() {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  consultarVehiculo():void{
+    this.estadoService.getAll()
+      .subscribe({
+         next: (data) => {
+           this.listaEstados = data;
+           this.dataSource = new MatTableDataSource(this.listaEstados);
+           this.dataSource.paginator = this.paginator;
+           this.dataSource.sort = this.sort;
+           console.log(data);
+         },
+         error: (e: any) => console.error(e)
+      });
+    
+  }
+
+  eliminarVehiculo(element:any){
+
+    swal.fire({
+      title: `¿Desea eliminar la factura #${element.nombre} la a nombre de ${element.descripcion}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        console.log(element._id);
+        this.estadoService.delete(element._id)
+          .subscribe({
+             next: (data) => {
+               this.consultarEstado();
+               console.log(data);
+              
+               this._snackbar.open('La factura eliminada correctamente','',{
+                  duration: 5000,
+                  horizontalPosition: 'center',
+                  verticalPosition: 'bottom'
+                });
+    
+             },
+             error: (e: any) => console.error(e)
+          });
+      } 
+
+    });
+    
+  } // fin del médoto de eliminar
+
+
+  modificarVehiculo(element:any){
+
+    swal.fire({
+      title: `¿Desea eliminar la factura #${element.nombre} la a nombre de ${element.descripcion}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, modificar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(element._id);
+        this.router.navigateByUrl(`dashboard/detalleEstado/${element._id}`);
+      } 
+
+    });
+
+  }// fin del método modificar
 
 }
