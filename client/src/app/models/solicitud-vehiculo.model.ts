69e970b749eadb6e45da63a15db60e94a3789a11
estebanmata <ematamoros1989@hotmail.com>
2022-04-03 15:57:49 -0600

    se agrega mensaje

--- client/src/app/models/solicitud-vehiculo.model.ts
+++ client/src/app/models/solicitud-vehiculo.model.ts
@@ -1,2 +1,11 @@
 export class SolicitudVehiculo {
+    _id?: any;
+    identificador?:Number;
+    tiempoLlegada?: Number;
+    tiempoDuracion?: Number;
+    costoServicio?: Number;
+    pagoLinea?:Number;
+    puntoSalida?: String;
+    puntoLlegada?:String;
+    comentario?: String;
 }
--- client/src/app/models/vehiculo.model.ts
+++ client/src/app/models/vehiculo.model.ts
@@ -1,2 +1,12 @@
 export class Vehiculo {
+    _id?: any;
+    identificador?:Number;
+    anno?: Number;
+    Modelo?: String;
+    placa?: String;
+    color?: String; 
+    puntuacion?: String;
+    estado?: String;
+    ubicacionActual?: String;
+
 }
