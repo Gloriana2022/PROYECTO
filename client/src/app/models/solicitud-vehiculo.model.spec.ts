f8909d474f82ae6cc6be314e79cf6994f5be37f0
Gloriana2022 <glorianavv13@hotmail.com>
2022-04-03 14:37:52 -0600

    Se agrego el mensaje

---
+++ client/src/app/models/solicitud-vehiculo.model.spec.ts
@@ -0,0 +1,7 @@
+import { SolicitudVehiculo } from './solicitud-vehiculo.model';
+
+describe('SolicitudVehiculo', () => {
+  it('should create an instance', () => {
+    expect(new SolicitudVehiculo()).toBeTruthy();
+  });
+});
