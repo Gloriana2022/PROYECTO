9591b4ab1b11a969732df157fdf9de21d9b38f73
Gloriana2022 <glorianavv13@hotmail.com>
2022-04-03 18:14:11 -0600

    Se agrego el mensaje

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { UsuarioService } from '../../service/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //Lista de Usuarios
+  listaUsuarios : Usuario[] = [];
+
+  //Configuración de la tabla
+  displayedColumns: string[] = ['numUsuario', 'nomUsuario', 'acciones'];
+  dataSource!:  MatTableDataSource<any>;
+
+
+  //Para la paginación
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor( private usuarioService : UsuarioService, private _snackbar: MatSnackBar, private router: Router ) { }
 
   ngOnInit(): void {
+    this.consultarUsuario();
+    
   }
+
+  ngAfterViewInit() {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  consultarUsuario():void{
+    this.usuarioService.getAll()
+      .subscribe({
+         next: (data) => {
+           this.listaUsuarios = data;
+           this.dataSource = new MatTableDataSource(this.listaUsuarios);
+           this.dataSource.paginator = this.paginator;
+           this.dataSource.sort = this.sort;
+           console.log(data);
+         },
+         error: (e: any) => console.error(e)
+      });
+    
+  }
+
+  eliminarUsuario(element:any){
+
+    Swal.fire({
+      title: `¿Desea eliminar el usuario #${element.numUsuario} la a nombre de ${element.nomUsuario}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        console.log(element._id);
+        this.usuarioService.delete(element._id)
+          .subscribe({
+             next: (data) => {
+               this.consultarUsuario();
+               console.log(data);
+              
+               this._snackbar.open('El usuario eliminado correctamente','',{
+                  duration: 5000,
+                  horizontalPosition: 'center',
+                  verticalPosition: 'bottom'
+                });
+    
+             },
+             error: (e: any) => console.error(e)
+          });
+      } 
+
+    });
+    
+  } // fin del médoto de eliminar
+
+
+  modificarUsuario(element:any){
+
+    Swal.fire({
+      title: `¿Desea eliminar el usuario #${element.numUsuario} con el nombre de ${element.nomUsuario}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, modificar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(element._id);
+        this.router.navigateByUrl(`dashboard/detalleUsuario/${element._id}`);
+      } 
+
+    });
+
+  }// fin del método modificar
+
 
 }
--- client/src/app/service/usuario.service.ts
+++ client/src/app/service/usuario.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario.model';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = `${environment.apiUrl}/usuarios`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Usuario> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
 }
